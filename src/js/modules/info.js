7fc758ca6ce7a69d00a7d1fe5544118c85e4a6fa
Роман Безуглий <romanbezuhlyi@MacBook-Pro-14.local>
2025-09-02 17:58:23 +0300

    added actions and action pages

--- src/js/modules/info.js
+++ src/js/modules/info.js
@@ -2,16 +2,16 @@ export function initSeeMore() {
 	const btn = document.querySelector('.info__block-content--btn')
 	const overlay = document.querySelector('.overlay')
 
+	if (!btn || !overlay) return // якщо елементів немає — вихід
+
 	btn.addEventListener('click', () => {
 		const isHidden = overlay.style.opacity === '0'
 
 		if (isHidden) {
-			// Показати оверлей
 			overlay.style.opacity = '1'
 			overlay.style.pointerEvents = 'all'
 			btn.textContent = 'Читать полностью'
 		} else {
-			// Сховати оверлей
 			overlay.style.opacity = '0'
 			overlay.style.pointerEvents = 'none'
 			btn.textContent = 'Скрыть'
